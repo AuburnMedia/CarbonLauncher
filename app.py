da742b51421218ef689850016cea55c740405217
AuburnMedia <nicholas.houghton@smseymour.catholic.edu.au>
2023-09-04 10:48:52 +1000

    Initial

---
+++ app.py
@@ -0,0 +1,14 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+        self.setMinimumSize(QSize(300,300))
+        self.setMaximumSize(1000,1000)
